1df5576b0a6f4ab978f976b86a08586689624e5a
Ayiba Cesario <ayibacesario@Ayibas-MacBook-Air.local>
2021-04-21 18:15:31 +0100

    added animation on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,67 @@
 //enable full page
-// const myFullPage = new fullpage('#fullpage',{
-//     licenseKey: '3310F724-FAB2489A-857B00AC-B4CA87B4',
-//     anchors:['home','about','experience','projects','contact'],
-// });
+const myFullPage = new fullpage('#fullpage',{
+    licenseKey: '3310F724-FAB2489A-857B00AC-B4CA87B4',
+    anchors:['home','about','experience','projects','contact'],
+    onLeave: function(origin, destination, direction){
+		var leavingSection = this;
+
+		if(origin.anchor == 'about' && direction =='down'){
+			const path = anime.path('#timeline path');
+            console.log('path', path);
+            const animation = anime({
+                targets: '.experience-item',
+                translateX: path('x'),
+                translateY: path('y'),
+                delay: anime.stagger(500, {from: 'last'}),
+                easing: 'linear',
+                duration: 2000,
+                loop: false
+                });
+            setTimeout(animation.pause, 2000);
+            const fade = function(){
+                const text = document.getElementsByClassName('experience-text');
+                for (const x of text) {
+                    x.style.opacity = "1"
+                    // x.style.animation = "fadeIn ease 2s";
+                    // x.style.animationFillMode = "forwards";
+                }
+                
+            };
+            setTimeout(fade, 2000);
+            // animation.restart();
+		}
+	},
+    afterLoad: function(origin, destination, direction){
+		var loadedSection = this;
+        console.log('experience section loaded')
+		if(origin.anchor == 'experience' && destination.anchor=='experience'){
+
+			const path = anime.path('#timeline path');
+            console.log('path', path);
+            const animation = anime({
+                targets: '.experience-item',
+                translateX: path('x'),
+                translateY: path('y'),
+                delay: anime.stagger(500, {from: 'last'}),
+                easing: 'linear',
+                duration: 2000,
+                loop: false
+                });
+            setTimeout(animation.pause, 2000);
+            const fade = function(){
+                const text = document.getElementsByClassName('experience-text');
+                for (const x of text) {
+                    x.style.opacity = "1"
+                    // x.style.animation = "fadeIn ease 2s";
+                    // x.style.animationFillMode = "forwards";
+                }
+                
+            };
+            setTimeout(fade, 2000);
+            // animation.restart();
+		}
+	},
+});
 
 //makes skills show after clicking on icon
 const skillsIcons = document.getElementsByClassName('skill-logo');
@@ -20,43 +79,3 @@ for(const i of skillsIcons){
             }
     });
 };
-
-
-////animate items on timeline
-// const path = anime.path('#timeline path');
-//                     console.log('path', path);
-//                     const animation = anime({
-//                         targets: '.experience-item',
-//                         translateX: path('x'),
-//                         translateY: path('y'),
-//                         delay: anime.stagger(500, {from: 'last'}),
-//                         easing: 'linear',
-//                         duration: 2000,
-//                         loop: false
-//                         });
-
-//                     setTimeout(animation.pause, 2000);
-
-
-//initiate timeline animation on scroll 
-const waypoint = new Waypoint({
-    element: document.getElementById('experience-section'),
-    handler: function(){
-        console.log('waypoints object', this);
-        const path = anime.path('#timeline path');
-                console.log('path', path);
-                const animation = anime({
-                    targets: '.experience-item',
-                    translateX: path('x'),
-                    translateY: path('y'),
-                    delay: anime.stagger(500, {from: 'last'}),
-                    easing: 'linear',
-                    duration: 2000,
-                    loop: false
-                    });
-                setTimeout(animation.pause, 2000);
-                // animation.restart();
-    }
-})
-
-
