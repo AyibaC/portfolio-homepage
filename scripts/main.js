d6d41c738f6a00ee1275c67f706b59c4d63798b0
Ayiba Cesario <ayibacesario@Ayibas-MacBook-Air.local>
2022-10-23 19:37:00 +0100

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,65 @@
 //enable full page
 const myFullPage = new fullpage('#fullpage',{
     licenseKey: '3310F724-FAB2489A-857B00AC-B4CA87B4',
-    anchors:['home','about','experience','projects','contact'],
+    anchors:['home','about','experience','projects', 'js-projects', 'css-projects', 'contact'],
     onLeave: function(origin, destination, direction){
 		var leavingSection = this;
 
-		if(origin.anchor == 'about' && direction =='down'){
-			const path = anime.path('#timeline path');
-            console.log('path', path);
-            const animation = anime({
-                targets: '.experience-item',
-                translateX: path('x'),
-                translateY: path('y'),
-                delay: anime.stagger(500, {from: 'last'}),
-                easing: 'linear',
-                duration: 2000,
-                loop: false
-                });
-            setTimeout(animation.pause, 2000);
-            const fade = function(){
-                const text = document.getElementsByClassName('experience-text');
-                for (const x of text) {
-                    x.style.opacity = "1"
-                    // x.style.animation = "fadeIn ease 2s";
-                    // x.style.animationFillMode = "forwards";
-                }
+		// if(origin.anchor == 'about' && direction =='down'){
+		// 	const path = anime.path('#timeline path');
+        //     console.log('path', path);
+        //     const animation = anime({
+        //         targets: '.experience-item',
+        //         translateX: path('x'),
+        //         translateY: path('y'),
+        //         delay: anime.stagger(500, {from: 'last'}),
+        //         easing: 'linear',
+        //         duration: 2000,
+        //         loop: false
+        //         });
+        //     setTimeout(animation.pause, 2000);
+        //     const fade = function(){
+        //         const text = document.getElementsByClassName('experience-text');
+        //         for (const x of text) {
+        //             x.style.opacity = "1"
+        //             // x.style.animation = "fadeIn ease 2s";
+        //             // x.style.animationFillMode = "forwards";
+        //         }
                 
-            };
-            setTimeout(fade, 2000);
-            // animation.restart();
-		}
+        //     };
+        //     setTimeout(fade, 2000);
+        //     // animation.restart();
+		// }
 	},
     afterLoad: function(origin, destination, direction){
 		var loadedSection = this;
         console.log('experience section loaded')
-		if(origin.anchor == 'experience' && destination.anchor=='experience'){
+		// if(origin.anchor == 'experience' && destination.anchor=='experience'){
 
-			const path = anime.path('#timeline path');
-            console.log('path', path);
-            const animation = anime({
-                targets: '.experience-item',
-                translateX: path('x'),
-                translateY: path('y'),
-                delay: anime.stagger(500, {from: 'last'}),
-                easing: 'linear',
-                duration: 2000,
-                loop: false
-                });
-            setTimeout(animation.pause, 2000);
-            const fade = function(){
-                const text = document.getElementsByClassName('experience-text');
-                for (const x of text) {
-                    x.style.opacity = "1"
-                    // x.style.animation = "fadeIn ease 2s";
-                    // x.style.animationFillMode = "forwards";
-                }
+		// 	const path = anime.path('#timeline path');
+        //     console.log('path', path);
+        //     const animation = anime({
+        //         targets: '.experience-item',
+        //         translateX: path('x'),
+        //         translateY: path('y'),
+        //         delay: anime.stagger(500, {from: 'last'}),
+        //         easing: 'linear',
+        //         duration: 2000,
+        //         loop: false
+        //         });
+        //     setTimeout(animation.pause, 2000);
+        //     const fade = function(){
+        //         const text = document.getElementsByClassName('experience-text');
+        //         for (const x of text) {
+        //             x.style.opacity = "1"
+        //             // x.style.animation = "fadeIn ease 2s";
+        //             // x.style.animationFillMode = "forwards";
+        //         }
                 
-            };
-            setTimeout(fade, 2000);
-            // animation.restart();
-		}
+        //     };
+        //     setTimeout(fade, 2000);
+        //     // animation.restart();
+		// }
 	},
 });
 
